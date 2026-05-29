9cc39b1348b33bab25a38ef7184afd154e05c5cf
eldergsilva <eldergsilva@gmail.com>
2026-05-29 10:25:56 -0300

    criado a estrutura do projeto

---
+++ 01/src/dump.sql
@@ -0,0 +1,16 @@
+create database biblioteca ;
+CREATE TABLE autores (
+    id SERIAL PRIMARY KEY,
+    nome TEXT NOT NULL,
+    idade INTEGER
+);
+
+ 
+
+CREATE TABLE livros (
+    id SERIAL PRIMARY KEY,                    
+    nome TEXT NOT NULL,
+    genero TEXT,                              
+    editora TEXT,
+    data_publicacao DATE,                     
+    id_autor INTEGER REFERENCES autores(id) 
