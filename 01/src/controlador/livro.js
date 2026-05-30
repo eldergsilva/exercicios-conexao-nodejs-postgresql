df30a09846786f9582dcf33654ebf8a3b559d778
eldergsilva <eldergsilva@gmail.com>
2026-05-30 10:05:53 -0300

    cadastrar  livro

--- 01/src/controlador/livro.js
+++ 01/src/controlador/livro.js
@@ -3,49 +3,27 @@ const listarLivros = async (req, res) => {
     const resultado = await pool.query('SELECT * FROM livros');
     return res.status(200).json(resultado.rows);
 }
-// 4 - Implemente uma tabela chamada `livros` contendo as seguintes características:
 
-// - Um identificador único do livro como chave primaria e auto incremento;
-// - O nome (obrigatório)
-// - O genero
-// - A editora
-// - A data de publicação no formato `YYYY-MM-DD`
-// - O identificador do autor responsável pelo livro
-
-// 5 - Implemente a funcionalidade de cadastrar um livro para um autor no banco de dados seguindo as especificações abaixo:
-
-// `POST /autor/:id/livro`
-
-// Exemplo de entrada
-
-// ```
-// {
-// 	"nome": "Backend PHP",
-// 	"genero": "Programação",
-// 	"editora": "Cubos Academy",
-// 	"data_publicacao": "2020-10-18"
-// }
 const cadastrarLivro = async (req, res) => {
-    // CREATE TABLE livros (
-    // id SERIAL PRIMARY KEY,                    
-    // nome TEXT NOT NULL,
-    // genero TEXT,                              
-    // editora TEXT,
-    // data_publicacao DATE,                     
-    // id_autor INTEGER REFERENCES autores(id)   
-    // );  
-
     const { id } = req.params;
     const { nome, genero, editora, data_publicacao } = req.body;
 
     if (!nome) {
         return res.status(400).json({ mensagem: 'O nome do livro é obrigatório' });
-    }       
+    }
+
     try {
+        const autor = await pool.query('SELECT * FROM autores WHERE id = $1', [id]);
+
+        if (autor.rows.length === 0) {
+            return res.status(404).json({ mensagem: 'Autor não encontrado' });
+        }
+
         const cadastrar = await pool.query(
             'INSERT INTO livros (nome, genero, editora, data_publicacao, id_autor) VALUES ($1, $2, $3, $4, $5) RETURNING *',
             [nome, genero, editora, data_publicacao, id]
         );
+
         return res.status(201).json(cadastrar.rows[0]);
     } catch (erro) {
         return res.status(500).json({ mensagem: 'Erro ao cadastrar livro' });
@@ -57,7 +35,8 @@ const cadastrarLivro = async (req, res) => {
 
 
 module.exports = {
-    listarLivros
+    listarLivros,   
+    cadastrarLivro
 }
 
 
--- 01/src/rotas.js
+++ 01/src/rotas.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const rotas = express.Router();  
 const { listarAutores, cadastrarAutor,buscarAutorPorId } = require('./controlador/autor');
-const { listarLivros } = require('./controlador/livro');
+const { listarLivros,cadastrarLivro } = require('./controlador/livro');
 
 rotas.get('/autores', listarAutores);
 rotas.post('/autores', cadastrarAutor);
 rotas.get('/autores/:id', buscarAutorPorId);
 rotas.get('/livros', listarLivros);
+rotas.post('/autor/:id/livro', cadastrarLivro);
 
 module.exports = rotas;
